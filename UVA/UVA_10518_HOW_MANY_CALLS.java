77fc01f5a15ad4af970b7255a5036e0786387287
Ahmed Diab <a7mediab977@gmail.com>
2019-06-12 22:20:10 +0200

    add UVA Solutions

--- UVA/UVA_10518_HOW_MANY_CALLS.java
+++ UVA/UVA_10518_HOW_MANY_CALLS.java
@@ -1,98 +1,91 @@
-package UVA;
-
-import java.io.*;
+package UVA ; 
 import java.util.*;
+import java.io.*;
 
-public class UVA_10518_HOW_MANY_CALLS {
-		
-	static int f(long n , int mod )
-	{
-		if(n == 1|| n == 0)
-			return 1 ; 
-		
-		return  (int) ((2*(n))%mod) ;
-	}
+public class UVA_10518_HOW_MANY_CALLS
+{
+    int [][] T ;
+    int [][] mult(int [][] A , int [][] B , int M)
+    {
+        int [][] res = new int [3][3] ;
+        for(int i = 0 ;i < 3 ; i++)
+            for(int j = 0 ;j < 3 ; j++)
+                for(int k = 0 ; k < 3 ; k++)
+                {
+                    res[i][j] += (int)(1L * A[i][k] * B[k][j] % M) ;
+                    res[i][j] %= M ;
+                }
+        return res ;
+    }
+    int [][] pow(int [][] A , long e , int M)
+    {
+        int [][] ans = new int [3][3];
+        for(int i = 0 ; i < 3 ; i++)ans[i][i] = 1 ;
 
-	public static void main(String[] args) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		PrintWriter out = new PrintWriter(System.out);
-		int cases = 1 ; 
-		while(true)
-		{
-			long n = sc.nextLong();
-			int mod = sc.nextInt();
-			
-			if(n == 0 && mod == 0)
-				break;
-			
-			out.printf("Case %d: %d %d %d\n", cases++ , n , mod , f(n, mod) );
-			
-			
-		}
-		
-		out.flush();
-		out.close();
-		
-		
-		
-		
-			
-	}
-	
-	static class Scanner {
-		StringTokenizer st;
-		BufferedReader br;
+        while(e > 0)
+        {
+            if((e & 1) == 1)ans = mult(ans , T , M) ;
+            T = mult(T , T , M) ;
+            e >>= 1 ;
+        }
+        return ans ;
+    }
+    int fib(int n)
+    {
+        if(n <= 1) return 1 ;
+        return fib(n - 1) + fib(n - 2) + 1;
 
-		public Scanner(InputStream s) {
-			br = new BufferedReader(new InputStreamReader(s));
-		}
+    }
+    void main() throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+//        System.out.println(fib(10));
+        for(int c = 1 ; ; c++)
+        {
+            long N = sc.nextLong() ;
+            int M = sc.nextInt() ;
+            if(N == 0 && M == 0) break;
+            T = new int [][]{{1 , 1 , 1} , {1 , 0 , 0} , {0 , 0 , 1}} ;
+            T = pow(T , N - 2 , M) ;
+            int ans = 0 ;
+            for(int i = 0 ;i < 3 ; i++)
+                for(int j = 0 ; j < 3 ; j++)
+                {
+                    ans +=  T[i][j] % M;
+                    ans %= M ;
+                }
+            if(N == 0 || N == 1)ans = 1 ;
+            out.printf("Case %d: %d %d %d\n" ,c , N , M ,  ans % M);
+        }
+        out.flush();
+        out.close();
+    }
 
-		public String next() throws IOException {
-			while (st == null || !st.hasMoreTokens())
-				st = new StringTokenizer(br.readLine());
-			return st.nextToken();
-		}
+    class Scanner
+    {
+        BufferedReader br;
+        StringTokenizer st;
 
-		public int nextInt() throws IOException {
-			return Integer.parseInt(next());
-		}
+        Scanner(InputStream in)
+        {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
 
-		public long nextLong() throws IOException {
-			return Long.parseLong(next());
-		}
+        String next() throws Exception
+        {
+            while (st == null || !st.hasMoreTokens())
+                st = new StringTokenizer(br.readLine());
+            return st.nextToken();
+        }
 
-		public String nextLine() throws IOException {
-			return br.readLine();
-		}
+        int nextInt() throws Exception { return Integer.parseInt(next()); }
 
-		public double nextDouble() throws IOException {
-			String x = next();
-			StringBuilder sb = new StringBuilder("0");
-			double res = 0, f = 1;
-			boolean dec = false, neg = false;
-			int start = 0;
-			if (x.charAt(0) == '-') {
-				neg = true;
-				start++;
-			}
-			for (int i = start; i < x.length(); i++)
-				if (x.charAt(i) == '.') {
-					res = Long.parseLong(sb.toString());
-					sb = new StringBuilder("0");
-					dec = true;
-				} else {
-					sb.append(x.charAt(i));
-					if (dec)
-						f *= 10;
-				}
-			res += Long.parseLong(sb.toString()) / f;
-			return res * (neg ? -1 : 1);
-		}
+        long nextLong() throws Exception { return Long.parseLong(next()); }
 
-		public boolean ready() throws IOException {
-			return br.ready();
-		}
+        double nextDouble() throws Exception { return Double.parseDouble(next());}
+    }
 
-	}
+    public static void main (String [] args) throws Exception {(new UVA_10518_HOW_MANY_CALLS()).main();}
 
 }
